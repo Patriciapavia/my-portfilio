2546db5e0444c70a81a12579ca483fc8e95f425a
patricia <patriciapavia236@gmail.com>
2020-06-13 16:51:24 +1000

    add more style

--- main.js
+++ main.js
@@ -28,3 +28,19 @@ $(document).ready(function () {
     duration: 1800,
   });
 });
+
+// portfolio
+
+function openTap(evt, cityName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
